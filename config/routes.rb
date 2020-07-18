59c54880d76dae9efc8fedc4c9378c5063b2a8d1
takanori_kaku <kaku.takanori.1026@gmail.com>
2020-07-18 15:09:30 +0000

    user_controller_追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  root 'static_pages#top'
+  
+  get 'users/new'
 
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'static_pages#top'
+  resources :users
+  
+  get    '/login', to: 'sessions#new'
+  post   '/login', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+
 end
